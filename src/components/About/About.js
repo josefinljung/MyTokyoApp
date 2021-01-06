76f0b990d6f5abbc0fef79b8603329edbc23fa4a
josefinljung <58325534+josefinljung@users.noreply.github.com>
2021-01-06 23:38:19 +0100

    Finished Inspiration page layout for tablet and phone size.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,4 +1,3 @@
-// import './Main.scss';
 import aboutus from '../../assets/aboutus.jpg'
 
 function About() {
@@ -7,7 +6,7 @@ function About() {
     <div className="abouttextcontainer">
       <h2 className="aboutheading">Hi! We’re the people behind this page, Mika & Sam!</h2>
       <p className="abouttext medium">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel lorem in magna feugiat scelerisque. Fusce a libero nisl. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vivamus eros metus, maximus id neque sed, sollicitudin lobortis eros. Morbi fermentum, dui ullamcorper fringilla blandit, est ligula condimentum tellus, non commodo metus mauris nec turpis. Nullam eleifend interdum dui vel accumsan. Mauris lorem sapien, imperdiet eget sem eget, mattis varius felis. Aliquam accumsan lectus in eros finibus sollicitudin. Duis pharetra tortor lacus, a rutrum risus placerat in. Maecenas vel ipsum dapibus, varius enim ullamcorper, vulputate dolor. Vivamus non est vel quam commodo tincidunt.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel lorem in magna feugiat scelerisque. Mauris lorem sapien, imperdiet eget sem eget, mattis varius felis. Aliquam accumsan lectus in eros finibus sollicitudin. Duis pharetra tortor lacus, a rutrum risus placerat in. Maecenas vel ipsum dapibus, varius enim ullamcorper, vulputate dolor. Vivamus non est vel quam commodo tincidunt.
       </p>
     </div>
     <div className="aboutimagecontainer">
--- src/components/Inspiration/Inspiration.js
+++ src/components/Inspiration/Inspiration.js
@@ -1,10 +1,79 @@
-// import './Main.scss';
+import fblogo from  '../../assets/facebook.svg';
+import iglogo from  '../../assets/instagram.svg';
+import letterlogo from '../../assets/email.svg';
+import aboutus from '../../assets/aboutus.jpg'
+import maplogo from '../../assets/map-marker.svg';
 
 function Inspiration() {
   return (
-    <h2 className="Inspiration">
-        This will be the inpiration page.
-    </h2>
+    <div className="inspirationcontainer">
+      {/* loopa inspo-grej kanske */}
+      <div className="inspirationsection">
+        <div className="inspirationtextsection">
+          <h2 className="inspirationheading">
+            {/* interpolera ut rubrik */}
+            This is a supercool shop.
+          </h2>
+          <p className="inspirationcopy big">
+            {/* interpolera ut brödtext */}
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus a lacinia ex. Nulla quis metus ac nulla sodales auctor eu vel augue. Donec ut sapien sit amet tellus pharetra vulputate. 
+          </p>
+          <div className="inspirationicons">
+            <span>
+              <img src={iglogo} alt="instagram" className="iglogo" />
+            </span>
+            <span>
+              <img src={fblogo} alt="facebook" className="fblogo" />
+            </span>
+            <span>
+              <img src={letterlogo} alt="letter" className="letterlogo" />
+            </span>
+            <span>
+              <img src={maplogo} alt="maplogo" className="maplogo" />
+            </span>
+          </div>
+        </div>
+        <div className="inspirationimage">
+          {/* interpolera ut inspobild */}
+          <img alt="inspiration" src={aboutus} className="test"></img>
+        </div>
+      </div> 
+      <div className="inspirationsection">
+        <div className="inspirationtextsection">
+          <h2 className="inspirationheading">
+            {/* interpolera ut rubrik */}
+            Here’s a list of our top five books about sustainable living.          </h2>
+          <p className="inspirationcopy big">
+            {/* interpolera ut brödtext */}
+            <ul>
+              <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+              <li>Nullam sagittis lorem ut quam porttitor consectetur.</li>
+              <li>In eu ante auctor justo efficitur pharetra.</li>
+              <li>Integer ut risus sed tortor ultrices pharetra rutrum sed elit.</li>
+              <li>Suspendisse et velit feugiat, lobortis massa non, consequat ligula.</li>
+            </ul>
+          </p>
+          <div className="inspirationicons">
+            <span>
+              <img src={iglogo} alt="instagram" className="iglogo" />
+            </span>
+            <span>
+              <img src={fblogo} alt="facebook" className="fblogo" />
+            </span>
+            <span>
+              <img src={letterlogo} alt="letter" className="letterlogo" />
+            </span>
+            <span>
+              <img src={maplogo} alt="maplogo" className="maplogo" />
+            </span>
+          </div>
+        </div>
+        <div className="inspirationimage">
+          {/* interpolera ut inspobild */}
+          <img alt="inspiration" src={aboutus} className="test"></img>
+        </div>
+      </div>   
+    </div>
   );
 }
 
